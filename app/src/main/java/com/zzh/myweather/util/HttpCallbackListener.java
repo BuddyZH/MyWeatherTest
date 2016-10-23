3b8ae91ba4458c7cdc132962ddb674712946b0fa
BuddyZH <zhanhong66@gmail.com>
2016-10-23 23:21:57 +0800

    util工具类“

---
+++ app/src/main/java/com/zzh/myweather/util/HttpCallbackListener.java
@@ -0,0 +1,9 @@
+package com.zzh.myweather.util;
+
+/**
+ * Created by ZZH on 2016/10/23.
+ */
+public class HttpCallbackListener {
+    void onFinish(String response);
+    void onError(Exception e);
+}
---
+++ app/src/main/java/com/zzh/myweather/util/Utility.java
@@ -0,0 +1,7 @@
+package com.zzh.myweather.util;
+
+/**
+ * Created by ZZH on 2016/10/23.
+ */
+public class Utility {
+}
